1a94181ceedd7b9b953e54d2fe16bc0eb5b0d76a
Patricia Rivera <patricia.rivera@patricia.rivera>
2020-09-06 15:55:13 -0300

    first commit - adding all the advance so far

---
+++ src/containers/Counter/Counter.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { connect} from 'react-redux';
+
+import CounterControl from '../../components/CounterControl/CounterControl';
+import CounterOutput from '../../components/CounterOutput/CounterOutput';
+
+class Counter extends Component {
+
+    render () {
+        return (
+            <div>
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter}  />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(strResult => (
+                        <li key={strResult.id} onClick={this.props.onDeleteResult}>{strResult.value}</li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state =>{
+    return {
+        ctr: state.counter,
+        storedResults: state.results
+    };
+}
+
+const mapDispatchToProps = dispatch => {
+    return{
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
+        onAddCounter: () => dispatch({type: 'ADD5', value:5}),
+        onSubtractCounter: () => dispatch({type: 'SUBTRACT5', value:5}),
+        onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
+        onDeleteResult: () => dispatch({type: 'DELETE_RESULT'})
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
---
+++ src/store/reducer.js
@@ -0,0 +1,37 @@
+const initialState = {
+    counter: 0,
+    results: []
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case 'INCREMENT':
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+        case 'DECREMENT':
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
+        case 'ADD5':
+            return {
+                ...state,
+                counter: state.counter + action.value
+            }
+        case 'SUBTRACT5':
+            return {
+                ...state,
+                counter: state.counter - action.value
+            }
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter})
+            }
+    }
+    return state;
+}
+
+export default reducer;
